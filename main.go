82bba2cc6cde40aa9a0409b17ac3495973e9b9d0
Gildas Le Nadan <gildas@endemic-systems.com>
2018-09-03 22:06:50 +1000

    go fmt

--- main.go
+++ main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-    "time"
-    "fmt"
-    "github.com/hashicorp/sentinel-sdk"
-    "github.com/hashicorp/sentinel-sdk/rpc"
-    "github.com/hashicorp/sentinel-sdk/framework"
+	"fmt"
+	"github.com/hashicorp/sentinel-sdk"
+	"github.com/hashicorp/sentinel-sdk/framework"
+	"github.com/hashicorp/sentinel-sdk/rpc"
+	"time"
 )
 
 func main() {
-    rpc.Serve(&rpc.ServeOpts{
-        ImportFunc: func() sdk.Import {
-            return &framework.Import{Root: &root{}}
-        },
-    })
+	rpc.Serve(&rpc.ServeOpts{
+		ImportFunc: func() sdk.Import {
+			return &framework.Import{Root: &root{}}
+		},
+	})
 }
 
 type root struct {
-    time time.Time
+	time time.Time
 }
 
 // framework.Root impl.
 func (m *root) Configure(raw map[string]interface{}) error {
-    if _, ok := raw["timestamp"]; !ok {
-        raw["timestamp"] = time.Now().Unix()
-    }
+	if _, ok := raw["timestamp"]; !ok {
+		raw["timestamp"] = time.Now().Unix()
+	}
 
-    v := raw["timestamp"]
-    timestamp, ok := v.(int64)
-    if !ok {
-        return fmt.Errorf("invalid timestamp type %T", v)
-    }
+	v := raw["timestamp"]
+	timestamp, ok := v.(int64)
+	if !ok {
+		return fmt.Errorf("invalid timestamp type %T", v)
+	}
 
-    m.time = time.Unix(timestamp, 0).UTC()
-    return nil
+	m.time = time.Unix(timestamp, 0).UTC()
+	return nil
 }
 
 // framework.Namespace impl.
 func (m *root) Get(key string) (interface{}, error) {
-    switch key {
-    case "minute":
-        return m.time.Minute(), nil
-    }
+	switch key {
+	case "minute":
+		return m.time.Minute(), nil
+	}
 
-    return nil, nil
+	return nil, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-    "os"
-    "testing"
+	"os"
+	"testing"
 
-    "github.com/hashicorp/sentinel-sdk"
-    plugintesting "github.com/hashicorp/sentinel-sdk/testing"
+	"github.com/hashicorp/sentinel-sdk"
+	plugintesting "github.com/hashicorp/sentinel-sdk/testing"
 )
 
 func TestMain(m *testing.M) {
-    exitCode := m.Run()
-    plugintesting.Clean()
-    os.Exit(exitCode)
+	exitCode := m.Run()
+	plugintesting.Clean()
+	os.Exit(exitCode)
 }
 
 func TestImport(t *testing.T) {
-    cases := []struct {
-        Name   string
-        Data   map[string]interface{}
-        Source string
-    }{
-        {
-            "month",
-            map[string]interface{}{"timestamp": 1495483674},
-            `main = subject.month.string == "September"`,
-        },
-    }
+	cases := []struct {
+		Name   string
+		Data   map[string]interface{}
+		Source string
+	}{
+		{
+			"month",
+			map[string]interface{}{"timestamp": 1495483674},
+			`main = subject.month.string == "September"`,
+		},
+	}
 
-    for _, tc := range cases {
-        t.Run(tc.Name, func(t *testing.T) {
-            plugintesting.TestImport(t, plugintesting.TestImportCase{
-                Config: tc.Data,
-                Source: tc.Source,
-            })
-        })
-    }
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			plugintesting.TestImport(t, plugintesting.TestImportCase{
+				Config: tc.Data,
+				Source: tc.Source,
+			})
+		})
+	}
 }
-
